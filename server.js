cb6bc5863b33766683aee0fb623c73922f1b7f3d
hicoderindia <hicoder.dolly@gmail.com>
2022-02-07 23:15:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.get("/", (req, res)=>{
-    console.log("hello world");
-    // res.status(200).send("Hey")
-    // res.status(500).json({message:"error"})   //passing status and msg
-    // res.render("server.js")  //download the file.
-    res.render("index", {text:"Dolly"})  //to render in perticular page.
-})
+app.get("/", (req, res) => {
+  console.log("hello world");
+  // res.status(200).send("Hey")
+  // res.status(500).json({message:"error"})   //passing status and msg
+  // res.render("server.js")  //download the file.
+  res.render("index", { text: "Dolly" }); //to render in perticular page.
+});
 
-app.listen("3000")
-
-
-
-
-
+app.listen("3000");
 
 //we dont use HTML page because you can't read that and understand.
